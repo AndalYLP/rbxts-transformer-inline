86e5a5e4d2c7515bf86eadd94c0444fab928f1d5
AndalYLP <andal089.az@gmail.com>
2025-01-27 12:40:58 -0500

    feat: adds support to custom logging packages

--- src/transformer/main.ts
+++ src/transformer/main.ts
@@ -1,6 +1,7 @@
 import ts from "typescript";
 
 import type { TransformContext } from ".";
+import { file } from "../symbols/$file";
 import { SymbolUtils } from "../utils/symbol";
 import { visitImportDeclaration } from "./import-declaration";
 
@@ -18,6 +19,8 @@ export class SymbolTransformer {
             return this.visitStatement(node)
         } else if (ts.isIdentifier(node)) {
             return this.visitIdentifier(node)
+        } else if (ts.isCallExpression(node)) {
+            return this.visitCallExpression(node)
         }
 
         return this.context.transform(node);
@@ -27,15 +30,64 @@ export class SymbolTransformer {
         const symbol = this.typeChecker.getSymbolAtLocation(node);
         if (!symbol) return node
 
-        const newNode = this.symbolUtils.isOurImport(symbol)
-        if (!newNode) return node
+        if (!this.symbolUtils.isCorrectImport(symbol)) return node
 
         return this.symbolUtils.transformSymbol(node)
     }
 
+    public visitPropertyAccessExpression(main: ts.CallExpression, node: ts.PropertyAccessExpression): ts.Node {
+        const { factory, config: { CustomLogger: { LogFunctions, PackageName } } } = this.context
+
+        if (!LogFunctions.includes(node.name.text)) return main
+
+        const symbol = this.typeChecker.getSymbolAtLocation(node.expression)
+        if (!symbol) return main
+
+        if (!this.symbolUtils.isCorrectImport(symbol, PackageName)) return main
+
+        const argument = main.arguments[0]
+        if (!argument) return main
+
+        let result
+        if (ts.isIdentifier(argument)) {
+            const originalMessage = argument.text
+            const fileResult = file[1](this.context, argument) as ts.StringLiteral
+
+            result = factory.createTemplateExpression(
+                factory.createTemplateHead(`${fileResult.text} `),
+                [factory.createTemplateSpan(factory.createIdentifier(originalMessage), factory.createTemplateTail(""))]
+            )
+        } else if (ts.isStringLiteral(argument)) {
+            const originalMessage = argument.text
+            result = file[1](this.context, argument as unknown as ts.Identifier) as ts.StringLiteral
+
+            result.text += ` ${originalMessage}`
+        } else if (ts.isTemplateExpression(argument)) {
+            const fileResult = file[1](this.context, argument as unknown as ts.Identifier) as ts.StringLiteral
+
+            result = factory.createTemplateExpression(
+                factory.createTemplateHead(`${fileResult.text} ${argument.head.text}`),
+                argument.templateSpans
+            )
+        }
+
+        return factory.createCallExpression(main.expression, undefined, [result!])
+    }
+
+    public visitCallExpression(node: ts.CallExpression): ts.Node {
+        const expression = node.expression
+
+        if (ts.isPropertyAccessExpression(expression)) {
+            if (this.context.config.CustomLogger.Enabled)
+                return this.visitPropertyAccessExpression(node, expression)
+        }
+
+        return this.context.transform(node);
+    }
+
     public visitStatement(node: ts.Statement): ts.Statement {
         if (ts.isImportDeclaration(node)) {
-            return visitImportDeclaration(this.context, node);
+            return visitImportDeclaration(this.context, node)
         }
 
         return this.context.transform(node);
